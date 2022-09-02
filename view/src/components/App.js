67e3e3bb015962e0f0c01268ad8af045570a3121
Cloud9NB <blackseeds10@hotmail.com>
2022-09-02 16:59:48 -0700

    creates basic navbar

--- view/src/components/App.js
+++ view/src/components/App.js
@@ -1,7 +1,20 @@
 import '../css/App.css';
+import TopNav from './TopNav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <TopNav />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='*' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
