484a1dc991c673cb74ab0a52b6a783b2a87426c8
Cloud9NB <blackseeds10@hotmail.com>
2022-09-02 16:31:31 -0700

    initial express.js backend commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8001;
+const path = require('path');
+const cors = require('cors');
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server, {
+  cors: '*',
+  methods: '*',
+});
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'view/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'view/build', 'index.html'));
+  });
+}
+
+app.listen(port, () => console.log(`Express listening on port: ${port} 👍`));
