0013efbd6e6a2926df7532975be0ef38231d5a4c
Cloud9NB <blackseeds10@hotmail.com>
2022-09-05 15:36:30 -0700

    creats dynamic routes with user logged in

--- view/src/components/App.js
+++ view/src/components/App.js
@@ -4,16 +4,24 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
+import { useSelector } from 'react-redux';
 
 const App = () => {
+  const user = useSelector(({ user }) => user);
+
   return (
     <div>
       <BrowserRouter>
         <TopNav />
         <Routes>
           <Route index element={<Home />} />
-          <Route exact path='/login' element={<Login />} />
-          <Route exact path='/signup' element={<Signup />} />
+          {!user && (
+            <>
+              <Route exact path='/login' element={<Login />} />
+              <Route exact path='/signup' element={<Signup />} />
+            </>
+          )}
+
           <Route path='*' element={<Home />} />
         </Routes>
       </BrowserRouter>
