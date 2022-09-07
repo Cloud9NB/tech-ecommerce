23114463c65fed2ec042e81203cc044d00c83c56
Cloud9NB <blackseeds10@hotmail.com>
2022-09-07 00:34:35 -0700

    creates a form and functionality of adding it to mongodb

---
+++ view/src/components/NewProductImages.js
@@ -0,0 +1,23 @@
+import '../css/components/NewProductImages.css';
+
+const NewProductImages = ({ image, images, setProduct }) => {
+  const handleRemoveImage = imgObj => {
+    const updatedImages = [
+      ...images.filter(image => image.public_id !== imgObj.public_id),
+    ];
+
+    setProduct(prev => ({ ...prev, images: updatedImages }));
+  };
+
+  return (
+    <div className='image-preview'>
+      <img src={image.url} alt='product' />
+      <i
+        className='fa fa-times-circle'
+        onClick={() => handleRemoveImage(image)}
+      ></i>
+    </div>
+  );
+};
+
+export default NewProductImages;
