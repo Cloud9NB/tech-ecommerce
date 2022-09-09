e2b84e0336f7ddac8f679d8b81e1bc74cb6c2fe9
Cloud9NB <blackseeds10@hotmail.com>
2022-09-09 00:02:48 -0700

    creats the feature product look

---
+++ view/src/components/home/ProductPreview.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Badge, Card } from 'react-bootstrap';
+import '../../css/components/home/ProductPreview.css';
+
+const ProductPreview = ({ _id, name, category, images }) => {
+  return (
+    <Link to={`/product/${_id}`} className='product-preview__link'>
+      <Card className='product-preview__card'>
+        <Card.Img
+          variant='top'
+          className='product-preview__img'
+          src={images[0].url}
+        />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Badge bg='warning' text='dark'>
+            {category}
+          </Badge>
+        </Card.Body>
+      </Card>
+    </Link>
+  );
+};
+
+export default ProductPreview;
