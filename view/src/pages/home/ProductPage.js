17c3e3c50ea6b7d7f61e467841c894e85057088a
Cloud9NB <blackseeds10@hotmail.com>
2022-09-11 17:10:03 -0700

    lays out the product info and button to add to cart or if admin then to edit product

--- view/src/pages/home/ProductPage.js
+++ view/src/pages/home/ProductPage.js
@@ -1,56 +1,55 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Badge, Col, Container, Row } from 'react-bootstrap';
-import AliceCarousel from 'react-alice-carousel';
+import {
+  Badge,
+  Button,
+  ButtonGroup,
+  Col,
+  Container,
+  Form,
+  Row,
+} from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-import Loading from '../../components/home/Loading';
+import Loading from '../../components/home/productPage/Loading';
+import SimilarProducts from '../../components/home/productPage/SimilarProducts';
+import ProductImages from '../../components/home/productPage/ProductImages';
 import '../../css/pages/home/ProductPage.css';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 const ProductPage = () => {
   const [state, setState] = useState({
     product: null,
     similar: null,
   });
-
+  const user = useSelector(({ user }) => user);
   const { id } = useParams();
 
   useEffect(() => {
-    axios.get(`/products/${id}`).then(({ data }) =>
-      setState(prev => ({
-        ...prev,
-        product: data.product,
-        similar: data.similar,
-      }))
-    );
+    axios
+      .get(`/products/${id}`)
+      .then(({ data }) =>
+        setState(prev => ({
+          ...prev,
+          product: data.product,
+          similar: data.similar,
+        }))
+      )
+      .catch(err => console.log(err));
   }, [id]);
 
   if (!state.product) {
     return <Loading />;
   }
 
-  const handleDragStart = e => {
-    e.preventDefault();
-  };
-
-  const images = state.product.images.map(({ url }) => (
-    <img
-      className='product__carousel--image'
-      src={url}
-      onDragStart={handleDragStart}
-      alt='product carousel'
-    />
-  ));
+  const customer = user && !user.isAdmin;
+  const admin = user && user.isAdmin;
 
   return (
     <Container className='pt-4 product-page__container'>
       <Row>
-        <Col lg={6}>
-          <AliceCarousel
-            mouseTracking
-            items={images}
-            controlStrategy='alternate'
-          />
-        </Col>
+        <ProductImages product={state.product} />
         <Col lg={6} className='pt-4'>
           <h1>{state.product.name}</h1>
           <p>
@@ -60,8 +59,30 @@ const ProductPage = () => {
           <p style={{ textAlign: 'justify' }} className='py-3'>
             <strong>Description:</strong> {state.product.description}
           </p>
+          {customer && (
+            <ButtonGroup style={{ width: '90%' }}>
+              <Form.Select
+                size='lg'
+                style={{ width: '40%', borderRadius: '0' }}
+              >
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+              </Form.Select>
+              <Button size='lg'>Add To Cart</Button>
+            </ButtonGroup>
+          )}
+
+          {admin && (
+            <Link to={`/product/${state.product._id}/edit`}>
+              <Button size='lg'>Edit Product</Button>
+            </Link>
+          )}
         </Col>
       </Row>
+
+      <SimilarProducts product={state.product} similar={state.similar} />
     </Container>
   );
 };
