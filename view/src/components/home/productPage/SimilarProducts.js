011cce301fcc2342ad963735d124f9331944dd54
Cloud9NB <blackseeds10@hotmail.com>
2022-09-13 15:58:38 -0700

    remove AliceCarousel for now

--- view/src/components/home/productPage/SimilarProducts.js
+++ view/src/components/home/productPage/SimilarProducts.js
@@ -1,4 +1,4 @@
-import AliceCarousel from 'react-alice-carousel';
+// import AliceCarousel from 'react-alice-carousel';
 import ProductPreview from '../ProductPreview';
 
 const SimilarProducts = ({ similar }) => {
@@ -10,6 +10,7 @@ const SimilarProducts = ({ similar }) => {
         <div
           className='item d-flex justify-content-center align-items-center flex-wrap'
           key={index}
+          data-value={index}
         >
           <ProductPreview {...sim} />
         </div>
@@ -17,21 +18,22 @@ const SimilarProducts = ({ similar }) => {
     });
   }
 
-  const responsive = {
-    0: { item: 1 },
-    568: { item: 2 },
-    1024: { item: 3 },
-  };
+  // const responsive = {
+  //   0: { item: 1 },
+  //   568: { item: 2 },
+  //   1024: { item: 3 },
+  // };
   return (
     <div className='my-4'>
       <h2>Similar Products</h2>
       <div className='d-flex justify-content-center align-items-center flex-wrap'>
-        <AliceCarousel
+        {similarProducts}
+        {/* <AliceCarousel
           mouseTracking
           items={similarProducts}
           responsive={responsive}
           controlStrategy='alternate'
-        />
+        /> */}
       </div>
     </div>
   );
