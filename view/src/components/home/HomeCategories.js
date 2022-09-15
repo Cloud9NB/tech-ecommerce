00e67d9847c4b16b517f5e89c7fdbd0ac828cd7a
Cloud9NB <blackseeds10@hotmail.com>
2022-09-15 09:37:17 -0700

    make the categories section on home for phone view

--- view/src/components/home/HomeCategories.js
+++ view/src/components/home/HomeCategories.js
@@ -1,17 +1,21 @@
+import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import '../../css/components/home/HomeCategories.css';
 
 const HomeCategories = ({ name, img }) => {
   return (
-    <Link to={`/category/${name.toLocaleLowerCase()}`}>
-      <div
+    <Link
+      to={`/category/${name.toLocaleLowerCase()}`}
+      className='home-category-link__container'
+    >
+      <Card
         className='category__tile'
         style={{
           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${img})`,
         }}
       >
         {name}
-      </div>
+      </Card>
     </Link>
   );
 };
