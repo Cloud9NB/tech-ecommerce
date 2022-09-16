4142fd25c91ff827735d8c5c2f0847a9098dcef8
Cloud9NB <blackseeds10@hotmail.com>
2022-09-16 16:56:17 -0700

    general cleanup

--- view/src/components/home/productPage/SimilarProducts.js
+++ view/src/components/home/productPage/SimilarProducts.js
@@ -1,39 +1,17 @@
-// import AliceCarousel from 'react-alice-carousel';
 import ProductPreview from '../ProductPreview';
 
-const SimilarProducts = ({ similar }) => {
-  let similarProducts = [];
+const SimilarProducts = ({ similar, productId }) => {
+  const filteredProducts = similar.filter(sim => sim._id !== productId);
 
-  if (similar) {
-    similarProducts = similar.map((sim, index) => {
-      return (
-        <div
-          className='item d-flex justify-content-center align-items-center flex-wrap'
-          key={index}
-          data-value={index}
-        >
-          <ProductPreview {...sim} />
-        </div>
-      );
-    });
-  }
+  const similarProducts = filteredProducts.map((sim, index) => (
+    <ProductPreview {...sim} key={index} />
+  ));
 
-  // const responsive = {
-  //   0: { item: 1 },
-  //   568: { item: 2 },
-  //   1024: { item: 3 },
-  // };
   return (
     <div className='my-4'>
       <h2>Similar Products</h2>
       <div className='d-flex justify-content-center align-items-center flex-wrap'>
-        {similarProducts}
-        {/* <AliceCarousel
-          mouseTracking
-          items={similarProducts}
-          responsive={responsive}
-          controlStrategy='alternate'
-        /> */}
+        {similar && similarProducts}
       </div>
     </div>
   );
--- view/src/pages/home/ProductPage.js
+++ view/src/pages/home/ProductPage.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Badge, Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import Loading from '../../components/home/productPage/Loading';
@@ -8,8 +9,8 @@ import SimilarProducts from '../../components/home/productPage/SimilarProducts';
 import ProductImages from '../../components/home/productPage/ProductImages';
 import AddToCart from '../../components/home/productPage/AddToCart';
 import EditProduct from '../../components/home/productPage/EditProduct';
-import '../../css/pages/home/ProductPage.css';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import '../../css/pages/home/ProductPage.css';
 
 const ProductPage = () => {
   const [state, setState] = useState({
@@ -19,7 +20,7 @@ const ProductPage = () => {
 
   const user = useSelector(({ user }) => user);
   const { id } = useParams();
-
+  console.log(state.product);
   useEffect(() => {
     axios
       .get(`/products/${id}`)
@@ -46,11 +47,13 @@ const ProductPage = () => {
         <ProductImages product={state.product} />
         <Col lg={6} className='pt-4'>
           <h1>{state.product.name}</h1>
-          <p>
-            <Badge bg='primary'>{state.product.category}</Badge>
-          </p>
+          <Link to={`/category/${state.product.category}`}>
+            <p>
+              <Badge bg='primary'>{state.product.category}</Badge>
+            </p>
+          </Link>
 
-          <p className='product__price'>{state.product.price}</p>
+          <p className='product__price'>${state.product.price}</p>
 
           <p style={{ textAlign: 'justify' }} className='py-3'>
             <strong>Description:</strong> {state.product.description}
@@ -62,7 +65,7 @@ const ProductPage = () => {
         </Col>
       </Row>
 
-      <SimilarProducts similar={state.similar} />
+      <SimilarProducts similar={state.similar} productId={state.product._id} />
     </Container>
   );
 };
