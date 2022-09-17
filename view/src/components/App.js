f825df9e292a6fd74665a346b4ded6ffcd64110f
Cloud9NB <blackseeds10@hotmail.com>
2022-09-17 15:23:04 -0700

    added the cart page where only customers can acess

--- view/src/components/App.js
+++ view/src/components/App.js
@@ -9,9 +9,11 @@ import ProductPage from '../pages/home/ProductPage';
 import CategoryPage from '../pages/home/CategoryPage';
 import '../css/App.css';
 import NotFound from '../pages/404';
+import CartPage from '../pages/customer/CartPage';
 
 const App = () => {
   const user = useSelector(({ user }) => user);
+  const customer = user && !user.isAdmin;
 
   return (
     <div>
@@ -28,6 +30,8 @@ const App = () => {
           <Route exact path={'/product/:id'} element={<ProductPage />} />
           <Route exact path='/new-product' element={<NewProduct />} />
           <Route exact path='/category/:category' element={<CategoryPage />} />
+
+          {customer && <Route exact path='/cart' element={<CartPage />} />}
 
           <Route path='*' element={<NotFound />} />
         </Routes>
