04eb588abfa167d5ba41cf36b29d99d0a047b4d0
Cloud9NB <blackseeds10@hotmail.com>
2022-09-23 21:25:43 -0700

    creates an order schema

---
+++ models/Orders.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema(
+  {
+    products: {
+      type: Object,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    status: {
+      type: String,
+      default: 0,
+    },
+    total: {
+      type: Number,
+      default: 0,
+    },
+    count: {
+      type: Number,
+      default: 0,
+    },
+    date: {
+      type: String,
+      default: new Date().toISOString().split('T')[0],
+    },
+    address: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    postalCode: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+  },
+  { minimize: false }
+);
+
+const Order = mongoose.model('Order', OrderSchema);
+
+module.exports = Order;
