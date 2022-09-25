6432636d2ec570999dcc69ffd251da9b5b54a573
Cloud9NB <blackseeds10@hotmail.com>
2022-09-24 23:08:44 -0700

    creates a get users orders route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
-const { signup, login, getOrders } = require('../controllers/users');
+const {
+  signup,
+  login,
+  getOrders,
+  getUsersOrder,
+} = require('../controllers/users');
 
 router.get('/', getOrders);
 router.post('/signup', signup);
 router.post('/login', login);
-
+router.get('/:id/orders', getUsersOrder);
 module.exports = router;
