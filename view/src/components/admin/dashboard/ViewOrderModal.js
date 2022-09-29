fa5b992bcc30c53c79d159f4998db1ba70ce6a42
Cloud9NB <blackseeds10@hotmail.com>
2022-09-29 13:54:31 -0700

    put the map into a variable

--- view/src/components/admin/dashboard/ViewOrderModal.js
+++ view/src/components/admin/dashboard/ViewOrderModal.js
@@ -3,27 +3,31 @@ import { Button, Modal } from 'react-bootstrap';
 const ViewOrderModal = ({ setState, show, orderToShow }) => {
   const handleClose = () => setState(prev => ({ ...prev, show: false }));
 
+  const customersOrder = orderToShow.map(order => (
+    <div
+      key={order._id}
+      className='order-details__container d-flex justify-content-around py-2'
+    >
+      <img
+        src={order.images[0].url}
+        style={{ maxWidth: 100, height: 100, objectFit: 'cover' }}
+        alt=''
+      />
+      <p>
+        <span>{order.count} x </span> {order.name}
+      </p>
+      <p>Price: ${Number(order.price) * order.count}</p>
+    </div>
+  ));
+
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Order details</Modal.Title>
       </Modal.Header>
-      {orderToShow.map(order => (
-        <div
-          key={order._id}
-          className='order-details__container d-flex justify-content-around py-2'
-        >
-          <img
-            src={order.images[0].url}
-            style={{ maxWidth: 100, height: 100, objectFit: 'cover' }}
-            alt=''
-          />
-          <p>
-            <span>{order.count} x </span> {order.name}
-          </p>
-          <p>Price: ${Number(order.price) * order.count}</p>
-        </div>
-      ))}
+
+      {customersOrder}
+
       <Modal.Footer>
         <Button variant='secondary' onClick={handleClose}>
           Close
