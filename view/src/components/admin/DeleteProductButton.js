1bf480459bf21dbd9c8f44980faca96425a5a009
Cloud9NB <blackseeds10@hotmail.com>
2022-10-01 16:08:32 -0700

    adds a navigate back to dashboard after deleting  a product on product page

--- view/src/components/admin/DeleteProductButton.js
+++ view/src/components/admin/DeleteProductButton.js
@@ -1,14 +1,18 @@
 import { Button } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { useDeleteProductMutation } from '../../services/appApi';
 
 const DeleteProductButton = ({ productName, productId }) => {
   const user = useSelector(({ user }) => user);
   const [deleteProduct, { isLoading }] = useDeleteProductMutation();
+  const navigate = useNavigate();
 
   const handleDeleteProduct = e => {
     if (window.confirm(`Are you sure you want to delete ${productName}?`))
-      deleteProduct({ productId, userId: user._id });
+      deleteProduct({ productId, userId: user._id }).then(() =>
+        navigate('/dashboard')
+      );
   };
 
   return (
