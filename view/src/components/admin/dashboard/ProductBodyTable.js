605ac24d8a2565ecea98625528f36cee597ccb9a
Cloud9NB <blackseeds10@hotmail.com>
2022-10-01 16:16:00 -0700

    creates an onlick to navigat to product page when clicking on images

--- view/src/components/admin/dashboard/ProductBodyTable.js
+++ view/src/components/admin/dashboard/ProductBodyTable.js
@@ -1,17 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import EditProductButton from '../../admin/EditProductButton';
 import DeleteProductButton from '../DeleteProductButton';
 
 const ProductBodyTable = ({ images, _id, name, price }) => {
+  const navigate = useNavigate();
+
+  const goToProductPage = () => navigate(`/product/${_id}`);
+
   return (
     <tr>
       <td>
         <img
+          onClick={goToProductPage}
           src={images[0].url}
           alt='product'
           style={{
             width: 100,
             height: 100,
             objectFit: 'cover',
+            cursor: 'pointer',
           }}
           className='dashboard-product__preview'
         />
