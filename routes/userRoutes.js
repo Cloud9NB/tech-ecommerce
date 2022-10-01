05ef66630d52c2bfc0c5f65877982101bf5cd52e
Cloud9NB <blackseeds10@hotmail.com>
2022-10-01 15:57:49 -0700

    added the new update notifications route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,10 +4,12 @@ const {
   login,
   getOrders,
   getUsersOrder,
+  updateNotifications,
 } = require('../controllers/users');
 
 router.get('/', getOrders);
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/:id/orders', getUsersOrder);
+router.post('/:id/updateNotifications', updateNotifications);
 module.exports = router;
