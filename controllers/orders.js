b1119c766d780b56523529a609434c9273299b4d
Cloud9NB <blackseeds10@hotmail.com>
2022-10-01 15:43:37 -0700

    adds the notification to the admin when customer creates an order to the database

--- controllers/orders.js
+++ controllers/orders.js
@@ -3,6 +3,7 @@ const User = require('../models/Users');
 
 module.exports = {
   createOrder: async (req, res) => {
+    const io = req.app.get('socketio');
     const { userId, cart, address, city, postalCode, country, phoneNumber } =
       req.body;
 
@@ -24,9 +25,21 @@ module.exports = {
 
       user.cart = { total: 0, count: 0 };
       user.orders.push(order);
+
+      const notification = {
+        status: 'unread',
+        message: `New order from ${user.name}`,
+        time: new Date(),
+      };
+      io.sockets.emit('new-order', notification);
+
+      const admin = await User.findOne({ isAdmin: true });
+      admin.notifications.unshift(notification);
+      admin.markModified('notifications');
+      await admin.save();
+
       user.markModified('orders');
       await user.save();
-
       res.status(200).json(user);
     } catch (error) {
       res.status(400).json(error.message);
@@ -45,12 +58,22 @@ module.exports = {
   updateStatus: async (req, res) => {
     const { ownerId } = req.body;
     const { id } = req.params;
+    const io = req.app.get('socketio');
 
     try {
       const user = await User.findById(ownerId);
 
       await Order.findByIdAndUpdate(id, { status: 'Shipped' });
       const orders = await Order.find().populate('owner', ['email', 'name']);
+
+      const notification = {
+        status: 'unread',
+        message: `Order: ${id} shipped with success`,
+        time: new Date(),
+      };
+      io.sockets.emit('notification', notification, ownerId);
+      user.notifications.unshift(notification);
+      await user.save();
 
       res.status(200).json(orders);
     } catch (error) {
